aedb6fd90b2905e5885b03c261e69d66a0409c78
jbilek007 <jbilek@redhat.com>
2016-01-04 08:34:01 +0100

    delete merging info

--- make_graph.sh
+++ make_graph.sh
@@ -24,11 +24,9 @@ done
 # - for ignoring first (start up) low numbers and final (during stopping server) low numbers - return 10th lowest number
 res=`cat $SERVER_LOG_INPUT | grep "HANDLED INVOCATIONS IN THE PREVIOUS SECOND" | sed 's/.*HANDLED INVOCATIONS IN THE PREVIOUS SECOND..//g' | sed 's/ ===//g' | sort -n | head -n 10 | tail -n 1`
 echo YVALUE=$res > $SERVER_MIN_LOG_OUTPUT
-<<<<<<< HEAD
 echo "server min log"
 echo YVALUE=$res
-=======
->>>>>>> 6570ecc641fbdd6fea03fbd02f7096330dd5c15f
+
 # maximal value
 res=`cat $SERVER_LOG_INPUT | grep "HANDLED INVOCATIONS IN THE PREVIOUS SECOND" | sed 's/.*HANDLED INVOCATIONS IN THE PREVIOUS SECOND..//g' | sed 's/ ===//g' | sort -n | tail -n 1`
 echo YVALUE=$res > $SERVER_MAX_LOG_OUTPUT
@@ -39,17 +37,8 @@ echo YVALUE=$res
 
 # example of line of client log (result-perf18.log)
 # 03:28:10,592 INFO  [com.redhat.qe.ejbperf.PerfTestRunner:130] collecting results.. clients=620; average latency=212ms; successful calls=10741; calls per client=17
-# interested data is "clients" 
+# interested data is "clients"
 
-<<<<<<< HEAD
-=======
-# DATA FROM CLIENT
-
-# example of line of client log (result-perf18.log)
-# 03:28:10,592 INFO  [com.redhat.qe.ejbperf.PerfTestRunner:130] collecting results.. clients=620; average latency=212ms; successful calls=10741; calls per client=17
-# interested data is "clients" 
-
->>>>>>> 6570ecc641fbdd6fea03fbd02f7096330dd5c15f
 # maximal count of clients
 res=`cat $CLIENT_LOG_INPUT | grep "collecting results.. clients=" | sed 's/.*collecting results.. clients=//g' | sed 's/; average.*//g' | sort -n | tail -n 1`
 echo YVALUE=$res > $CLIENT_LOG_OUTPUT
